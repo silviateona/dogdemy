b890fa4e03cddbcf452afcc47136d9dad3a6ba06
Silvia Teona <silviateona@gmail.com>
2019-06-01 03:04:40 -0400

    so far, so good! object inheritance

---
+++ app/php/guardian.php
@@ -0,0 +1,28 @@
+<?php
+class Guardian extends Person{
+	public $pets;
+
+	public function __construct(string $username,
+								string $firstName,
+								string $lastName,
+								string $email,
+								string $pswd,
+								bool $gender,
+								string $description,
+								int $petnum){
+		parent::__construct(
+			$username,
+			$firstName,
+			$lastName,
+			$email,
+			$pswd,
+			$gender,
+			$description
+		);
+		$this->pets=$petnum;
+	}
+
+	public function getPetnum(){
+		return $this->pets;
+	}
+}
--- public/playtest.php
+++ public/playtest.php
@@ -2,7 +2,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title></title>
+		<title>PLAYTEST - DEV</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="../styles/style.css">
@@ -11,9 +11,10 @@
 	<body>
 <?php 
 require_once('../app/php/person.php');
-$peep = new Person('booboo','Boo','Boobly','boo@beep.com','baa',false,'huic is me!');
+require_once('../app/php/guardian.php'); //TODO:refine the php document path for requires/includes
+$peep = new Guardian('booboo','Boo','Boobly','boo@beep.com','baa',false,'huic is me!',2);
 echo 'Hello, it\'s me, '.ucfirst($peep->getUsername()).', and my gender is: '.strtoupper($peep->getGender()).'!';
-
+echo "<br>I have ".$peep->getPetnum()." pets total!!";
 ?>
 	</body>
 </html>
